4c21285a1b52a5e431fd314dd608fe0259f56db8
Salvydas Lukosius <sall@w-ss.io>
2023-02-03 23:12:11 +0000

    modified:   .editorconfig 	modified:   functions/log-cmd

--- log-lines.plugin.zsh
+++ log-lines.plugin.zsh
@@ -1,12 +1,22 @@
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+# Standardized $0 Handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# Then ${0:h} to get plugin’s directory
+# Functions directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
+if [[ $PMSPEC != *f* ]]; then
+  fpath+=( "${0:h}/functions" )
+fi
 
-fpath+=( "${0:h}/functions" )
-path+=( "${0:h}/bin" )
+# Binaries directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#binaries-directory
+if [[ $PMSPEC != *b* ]]; then
+  path+=( "${0:h}/bin" )
+fi
 
-local to_load
-to_load=(${0:h}/functions/*)
+## Autoload functions
+typeset to_load; to_load=(${0:h}/functions/*)
 to_load=(${to_load#*functions/*})
 autoload $to_load
+unset to_load
